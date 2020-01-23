f53db8f5bb9be3b56ae78f1a299563184fada3b7
James McMahan <jamespatrickmc412@gmail.com>
2020-01-23 17:12:00 -0600

    Added BallIntake subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,7 @@
 
 package frc.robot;
 
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants.  This class should not be used for any other purpose.  All constants should be
@@ -16,4 +17,9 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    public static final class IntakeConstants {
+        public static final int IntakeMotor = 0;
+        public static final double BallIntakePercentOutput = 0.5;
+    }
+
 }
