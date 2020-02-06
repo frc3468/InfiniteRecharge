83edc7b62a21d21499c60e649176fb16c0c9f4fb
fastassonic <gabcall04@gmail.com>
2020-02-05 19:23:15 -0600

    Put area and distance in it right place

--- src/main/java/frc/robot/subsystems/camera.java
+++ src/main/java/frc/robot/subsystems/camera.java
@@ -7,26 +7,42 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-NetworkTableEntry Area;
-double conversion rate = 36.5740;
-public class camera extends SubsystemBase {
+
+
+  
+
+
+public class Camera extends SubsystemBase {
+  
+  double conversionrate = 36.5740;    
+  NetworkTableInstance table; 
+  NetworkTable cameraTable; 
+  NetworkTableEntry area;
+  //Replace mycamname with the name of the camera once we get that set up  
+
   /**
    * Creates a new ExampleSubsystem.
    */
-  public camera() {
-    
-    
+  public Camera() {
+    cameraTable = NetworkTableInstance.getTable("chameleon-vision").getSubTable("mycamname");
+    table = NetworkTableInstance.getDefault();
+    area = cameraTable.getEntery(targetArea);
   };
 
   @Override
-  public void periodic() {
+  public final void Periodic() {
     // This method will be called once per scheduler run
-
-  };
+    
+    };
   
-  public double distance() {
-
+  public double distance(){
+    
+    return double area * conversionrate;
   };
+
 };
