f4a4aeb0b41f21fc9fbce588cd1b3eaa29313a84
fastassonic <gabcall04@gmail.com>
2020-02-12 21:03:54 -0600

    fixed various issues revolving around network tables

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -58,9 +58,10 @@ public final class Constants {
         public static final int launchVelocity = 90; 
 
     }
-    public static final class CameraConstants {
-        public static final String cameraTableName = "camera_name";
-        public static final String chameleonName = "chameleon-vision";
+    public  final class CameraConstants {
+        // Make sure to look at this and possibly renable static at one point.  Cannot be refrenced in non static field
+        public final String cameraTableName = "camera_name";
+        public final String chameleonName = "chameleon-vision";
 
     }
 
