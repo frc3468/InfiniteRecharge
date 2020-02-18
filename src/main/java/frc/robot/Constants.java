63d654e48b0106084f4c6c94b8ce22c4bc5ae08c
NathanielMitchell <dragonfire82003@gmail.com>
2020-02-18 17:28:34 -0600

    adjusted CAN IDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,10 +17,10 @@ package frc.robot;
  */
 public final class Constants {
     public static final class DrivetrainConstants {
-        public static final int frontLeftDriveMotor = 0;
-        public static final int rearLeftDriveMotor = 1;
-        public static final int frontRightDriveMotor = 2;
-        public static final int rearRightDriveMotor = 3;
+        public static final int frontLeftDriveMotor = 1;
+        public static final int rearLeftDriveMotor = 2;
+        public static final int frontRightDriveMotor = 3;
+        public static final int rearRightDriveMotor = 4;
     }
 
     public static final class OIConstants {
