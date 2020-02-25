af4c7d9fab29dc5006c865da3721276694cf12a3
Gustave Michel III <gixxygamma@gmail.com>
2020-02-24 23:26:42 -0600

    Removed License from Arduino Sketch and added Library Info

--- arduino_gamepad/InfiniteRechargeOperatorConsoleGamepad/InfiniteRechargeOperatorConsoleGamepad.ino
+++ arduino_gamepad/InfiniteRechargeOperatorConsoleGamepad/InfiniteRechargeOperatorConsoleGamepad.ino
@@ -1,19 +1,8 @@
 //  3468 MAGNATech's Custom Gamepad for the 2020 FRC Game Infinite Recharge
-//  NOTE: This sketch file is for use with the Arduino Pro Micro
-//  Copyright (C) 2020  Gustave Michel III <gustave@michel.com>
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//  For use with the Arduino Pro Micro (and other ATmega32u4 Microcontrollers)
+//  This sketch makes use of the following Libraries:
+//    https://github.com/MHeironimus/ArduinoJoystickLibrary/tree/version-2.0
+//  Written by: Gustave Michel III <gustave@michel.com>
 
 #include <Joystick.h>
 
