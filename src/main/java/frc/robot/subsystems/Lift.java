4df5f95efb11e6ca56de1a4777f239870e73472e
NathanielMitchell <dragonfire82003@gmail.com>
2020-03-06 09:32:39 -0600

    Tuning of several things

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -15,6 +15,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LiftConstants;
 
@@ -71,6 +72,8 @@ public class Lift extends SubsystemBase {
 
   @Override
   public void periodic() {
+    SmartDashboard.putBoolean("Fwd Hook Limit", isForwardLimitSwitch());
+    SmartDashboard.putBoolean("Rev Hook Limit", isReverseLimitSwitch());
     // This method will be called once per scheduler run
   }
 }
